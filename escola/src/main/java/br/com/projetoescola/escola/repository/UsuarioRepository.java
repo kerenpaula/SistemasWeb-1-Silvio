4b5b6dd296526352df55a0898b6476463295417d
Kéren <keren.paula@etec.sp.gov.br>
2025-11-24 20:42:36 -0300

    temos que finalizar a tela de login

--- escola/src/main/java/br/com/projetoescola/escola/repository/UsuarioRepository.java
+++ escola/src/main/java/br/com/projetoescola/escola/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package br.com.projetoescola.escola.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.projetoescola.escola.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByLoginUsuario(String loginUsuario);
     
 }
 
